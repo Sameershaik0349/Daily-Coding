6546e529d4ddf3d7e853fdc6dea468839244ab04
Sameershaik0349 <sksameer46644@gmail.com>
2026-01-23 01:23:40 +0530

    new

--- python/day21 while.py
+++ python/day21 while.py
@@ -19,8 +19,75 @@
 #     print()
 # for i in range (5,0,-1):
 #     print("*" *i)
+# for i in range(70,65,-1):
+#     for y in range(65,i + 1):
+#         print(chr(y),end =" ")
+#     print()
+# "while loop"------ until this conition is true it work as loop -----if we know the no of itearations then use for loop if dont know use while***
+# a =1
+# while a<=10:
+#     print(a)
+#     a+=1
 
-for i in range(1,6):
-    for y in range(1,i+1):
-        print(y,end =" ")
-    print()
+
+# a= -10     
+# while a<=-1:
+#     print(a)
+#     a+=1
+
+# a =123
+# while a ==123:
+#     a=str(a)
+#     print(a[::-1])
+
+# a =121
+# b=a
+# rev=0
+# while a>0:
+#     d=a%10
+#     rev= rev * 10 + d
+#     a//=10
+# if b == rev:
+#     print("its palindrome")
+# else :
+#     print("not palindrom")
+# else:
+#     print("its not a palindrom")
+#     if a == rev:
+#         print("its palindrom")
+# else :
+#     print("its not a palindrom")
+
+
+
+# a =24356
+# # h=a
+# count=0
+# # h=a
+# new=0
+
+# while a>0:
+#     d=a%10
+#     new+=d
+#     a//=10
+#     if new%d==0:
+#         count+=1
+# if count==1:
+#     print("its a prime")
+# else:
+#     print("not a prime")
+# print(count)
+
+num = 4
+i = 1
+count = 0
+
+while i <= num:
+    if num % i == 0:
+        count += 1
+    i += 1
+
+if count == 2:
+    print("Prime number")
+else:
+    print("Not a prime number")
