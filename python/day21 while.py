613ecdc21e437bba8a8e5750cb5c014f7a39a209
Sameershaik0349 <sksameer46644@gmail.com>
2026-01-22 23:10:21 +0530

    new

---
+++ python/day21 while.py
@@ -0,0 +1,17 @@
+# for i in range(1,5):
+#     for y in range(1,11):
+#         print(i,'x',y,'=', i * y)
+#     print()
+
+# n ="saa"
+# count=""
+# for i in (n[::-1]):
+#     count+=i
+# if count == n:
+#     print("its palindrom")
+# else:
+#     print("not a palindrom")  
+# print(count)
+    
+
+
