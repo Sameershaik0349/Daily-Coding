5ab2182103afaacf6ebd6c2d9beb0d3ab2ada98c
Sameershaik0349 <sksameer46644@gmail.com>
2026-01-22 23:48:24 +0530

    new

--- python/day21 while.py
+++ python/day21 while.py
@@ -13,5 +13,14 @@
 #     print("not a palindrom")  
 # print(count)
     
+# for x in range (1,6):
+#     for y in range (1,3):
+#         print("*" *x , "*" * y,end =" ")
+#     print()
+# for i in range (5,0,-1):
+#     print("*" *i)
 
-
+for i in range(1,6):
+    for y in range(1,i+1):
+        print(y,end =" ")
+    print()
