399e78868da1df751641983dd8ab269935d54d26
Sameershaik0349 <sksameer46644@gmail.com>
2026-01-23 17:24:01 +0530

    day22 problems

---
+++ python/day22.py
@@ -0,0 +1,90 @@
+# user =input('enter password :-')
+# c_pass = "Tobi27"
+# while user!=c_pass:
+#     print("wrong passowrd")
+#     user = input("enter password :-")
+# print("login succesfull Sameer")
+# user=int(input("enter positive num :-"))
+# while user<0:
+#     print("you entered neg num")
+#     user=(int(input("enter pos num :-")))
+# print("your num",user)
+# e =12345
+# temp=0
+# while e>0:
+#     d=e%10
+#     e//=10
+#     if d %2==0:
+#         temp=temp+d
+# print(temp)
+
+# f =34567
+# # g=1
+# sum_prime=0
+
+# while f >0:
+#     cou=0
+#     d=f%10
+#     f//=10
+#     for y in range(1,d):
+#         if d%y==0:
+#             cou+=1
+#     if cou==1:
+#        sum_prime =sum_prime+d
+# # print(sum_prime)
+
+
+
+# w =1
+# while w<21:
+#     print(w)
+#     w+=1
+
+
+# w =1
+# while w<51:
+#     if w%2==0:
+#         print(w)
+#     w+=1
+
+
+# w = 10
+# while w >0:
+#     # if w % 2!=0:
+#     print(w**2)
+#     w -=1
+
+
+
+
+# n=int(input("enter  10"))
+# sum=0
+# while n>=0:
+#     # if w>0:
+#     sum += n
+#     n-=1
+# print(sum)
+
+
+
+# w =123
+# len=0
+# while w>0:
+#     d=w%10
+#     w//=10
+#     len+=d
+# print(len)    
+
+
+# w =5
+# d=1
+# while d<11:
+#         print(w,"x",d,"=",w*d)
+#         d+=1
+# w =5
+# fac=1
+# while w>0:
+#     fac = fac*w
+#     w-=1
+# print(fac)
+    
