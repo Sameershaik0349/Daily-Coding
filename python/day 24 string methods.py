3341d7406f92bc1ed969f18d00a6ff61704435d1
Sameershaik0349 <sksameer46644@gmail.com>
2026-01-29 03:21:58 +0530

    day 24

---
+++ python/day 24 string methods.py
@@ -0,0 +1,21 @@
+# p = "wewq"
+# for i in range (len(p)):
+#     print(p[i])
+
+
+# p = 'python'
+# c=0
+# up=""
+# for x in p:
+#     if ord(x) >=97 and ord(x) <=122:
+#         c = ord(x) - 32
+#         up += chr(c)
+# print(up)
+
+# p ="PYTHON"
+# for i in p:
+#     if ord(i) >=65 and ord(i)<=91:
+#         d = ord(i) +32
+#         print(chr(d))
+
+
