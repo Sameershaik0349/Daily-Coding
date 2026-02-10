7271ca5819248f93fefdcd468c45b7def1094fba
Sameershaik0349 <sksameer46644@gmail.com>
2026-02-10 08:19:58 +0530

    sql class

---
+++ sql/day 35 nested sub qurie.sql
@@ -0,0 +1,78 @@
+-- "nested sub quires"
+show DATABASES;
+create DATABASE sub_quries;
+use sub_quries;
+
+CREATE TABLE Emp (
+    EmpID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Department VARCHAR(50),
+    Salary DECIMAL(10,2),
+    Age INT,
+    City VARCHAR(50)
+);
+
+INSERT INTO Emp (EmpID, Name, Department, Salary, Age, City) VALUES
+(1, 'Amit', 'HR', 35000, 29, 'Delhi'),
+(2, 'Sneha', 'Finance', 48000, 32, 'Mumbai'),
+(3, 'Ravi', 'IT', 55000, 28, 'Bangalore'),
+(4, 'Priya', 'Sales', 40000, 30, 'Chennai'),
+(5, 'Karan', 'Finance', 60000, 35, 'Delhi'),
+(6, 'Meena', 'HR', 30000, 26, 'Pune'),
+(7, 'Suresh', 'IT', 70000, 40, 'Hyderabad'),
+(8, 'Divya', 'Sales', 42000, 27, 'Mumbai'),
+(9, 'Vikram', 'Finance', 65000, 36, 'Bangalore'),
+(10, 'Nisha', 'IT', 72000, 31, 'Delhi'),
+(11, 'Rohit', 'HR', 31000, 25, 'Chennai'),
+(12, 'Pooja', 'Sales', 38000, 29, 'Pune'),
+(13, 'Anil', 'Finance', 58000, 34, 'Hyderabad'),
+(14, 'Neha', 'IT', 64000, 33, 'Mumbai'),
+(15, 'Rajesh', 'Sales', 45000, 37, 'Delhi'),
+(16, 'Komal', 'HR', 33000, 28, 'Bangalore'),
+(17, 'Deepak', 'Finance', 52000, 30, 'Chennai'),
+(18, 'Swati', 'IT', 76000, 38, 'Pune'),
+(19, 'Arjun', 'Sales', 47000, 29, 'Hyderabad'),
+(20, 'Lakshmi', 'Finance', 61000, 32, 'Delhi'),
+(21, 'Manoj', 'IT', 69000, 36, 'Bangalore'),
+(22, 'Sakshi', 'Sales', 39000, 26, 'Mumbai'),
+(23, 'Harish', 'HR', 29500, 24, 'Chennai'),
+(24, 'Kavita', 'Finance', 57000, 35, 'Hyderabad'),
+(25, 'Sunil', 'IT', 73000, 39, 'Delhi'),
+(26, 'Ramesh', 'Sales', 46000, 33, 'Pune'),
+(27, 'Jyoti', 'Finance', 59000, 31, 'Bangalore'),
+(28, 'Ashok', 'IT', 71000, 34, 'Mumbai'),
+(29, 'Tanvi', 'Sales', 41000, 27, 'Delhi'),
+(30, 'Gaurav', 'HR', 34000, 29, 'Hyderabad');
+
+
+
+
+-- find sec max salary 
+select * from emp where salary =
+(select max(salary) from emp where salary<
+(select max(salary)from emp));
+
+-- third high salaary
+SELECT * from emp where salary =
+(select max(salary) from emp where salary <
+(select max(salary) from emp where salary <
+(SELECT max(salary) from emp)));
+
+-- fro specifc salaary
+select salary from emp order by salary desc limit 19,1;
+
+
+
+select * from emp where department =
+(SELECT department from emp where salary =
+(select max(salary) from emp where salary >
+(select avg(salary) from emp)));
+
+-- "corelated quriesssss" ------- will take help of outer qureiy to form condition
+
+-- when groupin of values given for less than or greater quriey sure use correlated qurie
+
+SELECT * from emp as e where salary>
+(SELECT avg(salary) from emp where department=e.department);
+SELECT department,avg(salary) from emp group by department;
+select * from emp;
