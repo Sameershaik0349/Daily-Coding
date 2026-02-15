8fc2adaa8c9e6c301005771b65aec7e9a2e1e298
Sameershaik0349 <sksameer46644@gmail.com>
2026-02-15 22:38:19 +0530

    practice

--- sql/day 33 joins.sql
+++ sql/day 33 joins.sql
@@ -2,4 +2,30 @@
 show DATABASES;
 create DATABASE self_join;
 
-create TABLE cus1(id int PRIMARY KEY)
+create TABLE cus1(id int PRIMARY KEY);
+
+use self_join;
+show tables;
+
+use 84r;
+
+use joins;
+show tables;
+
+
+select * from  customers;
+
+select * from orders;
+-- with out condition  -----cross
+select * from customers join orders on customers.cus_id=orders.cus_id;
+-- with condition  ---inner join
+SELECT orders.ord_id,order_date,customers.first_name from customers join orders on customers.cus_id =orders.cus_id where customers.first_name ='rahul';
+
+
+--  self join  --doin in same table 
+-- with out condition
+select * from customers join customers as c;
+
+
+-- natural JOIN
+
