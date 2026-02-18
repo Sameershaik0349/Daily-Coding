456c6a6f5eb339b0fe18e438586b660ee5700b8d
Sameershaik0349 <sksameer46644@gmail.com>
2026-02-18 16:32:10 +0530

    pyhton def

---
+++ python/day37 funtions.py
@@ -0,0 +1,13 @@
+"Functions"  # if we wnat to reuse the code in 200 lines codes then we can use functions
+
+def even():
+    for i in range(2,40,2):
+        print(i)
+even()
+
+
+even() # reusing the function  
+
+
+
+
