0f1a9a8a28359e4d24c7f105f1f8522b28cdc6ae
Sameershaik0349 <sksameer46644@gmail.com>
2026-03-20 23:58:42 +0530

    abstraction

--- python/DAY 49/project.py
+++ python/DAY 49/project.py
@@ -1,11 +1,11 @@
 class account:
     def details(self,user_name,user_acc_no,user_acc_bal):
-        print("=============deatails===============")
+        print("=============details===============")
         self.user_name=user_name
         self.user_acc_no=user_acc_no
         self.user_acc_bal=user_acc_bal
         print(user_name, user_acc_no, user_acc_bal)
-        print("=============deatails===============")
+        print("=============details===============")
         
     def deposit(self,amount):
         if amount>0:
@@ -14,36 +14,39 @@ class account:
             print("deposited succesfull:-",self.user_acc_bal)
             print("=============deposit===============")
     def withdraw(self,amount):
-        if self.user_acc_bal<amount:
-            print("insufficent bal :-",self.user_acc_bal)
+        if amount>0:
+            if self.user_acc_bal<amount:
+                print("insufficent bal :-",self.user_acc_bal)
+            else:
+                self.user_acc_bal-=amount
+                print("=============withdraw===============")
+                print("withdraw successfull:-",self.user_acc_bal)
+                print("=============withdraw===============")
         else:
-            self.user_acc_bal-=amount
-            print("=============with draw===============")
-            print("withdrww sucess full:-",self.user_acc_bal)
-            print("=============with draw===============")
+                print("Please enter valid amount")
     def check_bal(self):
         print(self.user_acc_bal)
 
-
-user_name=input('enter your name :-')
-user_acc_no=int(input("enter your acc number:-"))
-user_acc_bal=float(input("emter initial bal"))
+import random;
+user_name=input('Enter your name :- ')
+user_acc_no= print(random.random)
+user_acc_bal=float(input("Enter initial balance :- "))
 obj=account()
 obj.details(user_name, user_acc_no, user_acc_bal)
 while True:
-    print("if for deposit press 1")
-    print("if for with draw press 2")
+    print("For deposit press 1")
+    print("if for withdraw press 2")
     print("if for check_bal press 3")
     print("if for details press 4")
     print("if exit press 5")
 
-    procces=int(input("enter a process num"))
+    procces=int(input("Enter a process number :-"))
     if procces==1:
-        amount=float (input("enter a amount"))
+        amount=float (input("Enter a amount :- "))
         obj.deposit(amount)
 
     elif procces ==2:
-            amount=float(input("enter withdraw amount"))
+            amount=float(input("Enter withdraw amount :-"))
             obj.withdraw(amount)
     elif procces==3:
         obj.check_bal()
@@ -52,7 +55,7 @@ while True:
         obj.details(user_name, user_acc_no, user_acc_bal)
          
     elif procces ==5:
-        print("thank you for visting")
+        print("Thank you for visting")
         break
     
 
---
+++ python/DAY 50/Abstraction.py
@@ -0,0 +1,4 @@
+# ABSTARCTION
+
+# IT WILL ONLY SHOW THE OBJECT AND HIDE THE INTERNAL PROCESS
+# ex:- atm machine  its hide internal moeny process😂
