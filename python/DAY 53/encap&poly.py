7e61969b24ad1bee60b08bb364997eaf55409725
Sameershaik0349 <sksameer46644@gmail.com>
2026-03-25 03:29:38 +0530

    abstraction

---
+++ python/DAY 53/encap&poly.py
@@ -0,0 +1,22 @@
+"ENCAPSULATION"
+
+'''
+METHODS:
+sET ---for inseting
+give paramter in set
+get --for get data 
+dont give parametr in get
+'''
+
+" Polymorphism"
+'''
+🔷 What is Polymorphism?
+
+Polymorphism = One thing → Many forms
+
+👉 Same method name behaves differently depending on the object.
+
+
+'''
+
+"In OOP, polymorphism allows objects of different classes to respond to the same method name in different ways."
---
+++ python/QR code.py/qr.py
@@ -0,0 +1,15 @@
+# import qrcode
+# qr =qrcode.make("https://www.instagram.com/reel/DWJFnhVjYqv/?igsh=MWxhOW10azhwYmcwMA==")
+# qr.save ('img.png')
+# qr.show()
+
+
+
+import qrcode
+
+qr =qrcode.QRCode(box_size=100)
+qr.add_data("hii")  #to add data
+qr.make()
+img=qr.make_image(fill_color="blue")
+img.save ("img.png")
+img.show()
