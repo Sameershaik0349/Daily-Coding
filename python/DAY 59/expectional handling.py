52468d31f7b45691324de9a1758a3298c50b3924
Sameershaik0349 <sksameer46644@gmail.com>
2026-04-09 17:58:41 +0530

    js

---
+++ python/DAY 59/expectional handling.py
@@ -0,0 +1,69 @@
+# exeptional handling////
+'''to find error easily
+ 
+
+try ---for risky code
+     ----and logic on code 
+
+'''
+
+
+# errors
+
+# exception handling
+
+'''name
+type error
+value
+file not
+attrib
+key'''
+
+# 1.NAME ERROR
+'''
+name = "asdfgfhng"
+print(pin) ---its called name error
+'''
+
+# 2.TYPE ERROr
+'''s=10+"pyhton"
+print(s) ---type error
+''' 
+
+"value error"
+
+'''try ---for risky code
+     ----and logic on code
+
+     
+exception--- 
+        it is like else if the the try block had error this expection will run for not crashimg the code  
+     
+     
+     '''
+
+
+try:
+    name="python"
+    print(pin)
+# except Exception as p:
+#     print(p)     when if we dont know erroe
+
+except NameError:
+    print('name error') #---if we know the error
+
+except ZeroDivisionError():
+    print("zerodiverror") 
+
+
+# except AttributeError():#uses in objects and classes
+
+
+# ==============degfault & expect=============
+
+'''deafault
+
+print("default error")
+its must in last
+'''
+
