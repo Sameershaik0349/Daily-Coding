6b293afadf4afd1e96c2aa5ceae23d293a1bcf36
Sameershaik0349 <sksameer46644@gmail.com>
2026-04-13 09:53:42 +0530

    js

---
+++ JS/DAY 5/app.js
@@ -0,0 +1,17 @@
+// let i =1
+// while (i<10){
+//     console.log(i)
+//     i++;
+// }
+
+
+
+let person={
+    loc:'hyd',
+    age:22,
+    marks:33
+}
+
+for(let x in person){
+    console.log(person[x])
+}
