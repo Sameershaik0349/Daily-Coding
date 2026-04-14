f6cc9f935dd87cec6c013ed07f397c89d8692ed9
Sameershaik0349 <sksameer46644@gmail.com>
2026-04-14 09:52:19 +0530

    gen & csv

---
+++ JS/DAY 6/app.js
@@ -0,0 +1,70 @@
+// let data =["sameer","ajay","kumar"]
+// console.log(data.length)
+// // console.log(data.sort())
+// console.log(data.reverse())
+// -----------------------------------------------
+//function declaration
+//function without papameter
+
+// function data(){
+//     console.log("Functions")
+// }
+
+
+// data()
+// ------------------------------------------------
+// function expression
+// ananymous function
+
+// let data=function(){
+//     console.log("this is function expression")
+// }
+// data()
+
+// ---------------------------------------------------------------
+
+
+// function with parameter
+
+// function hello(c,d){
+//     console.log(c+d)
+//     console.log(c-d)
+// }
+
+// hello(5,6)
+
+// ----------------------------------
+//postional urgument
+
+// function hello(a,b=8){
+//     console.log(a,b)
+// }
+
+// hello(7)
+
+
+
+// tassk
+
+function hello(c,d){
+    console.log(c+d)
+    console.log(c-d)
+}
+
+hello(5,6)
+
+
+
+let dat=function(a,b){
+    console.log(a*b)
+    console.log(a/b)
+}
+dat(2,)
+
+
+
+function da(){
+     console.log("sameer")
+}
+
+da()
