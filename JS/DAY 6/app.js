6f22cc23123af2455425895e573af4dca4fc748a
Sameershaik0349 <sksameer46644@gmail.com>
2026-04-15 10:02:36 +0530

    gen & csv

--- JS/DAY 6/app.js
+++ JS/DAY 6/app.js
@@ -6,6 +6,9 @@
 //function declaration
 //function without papameter
 
+// const { data } = require("framer-motion/client")
+
+
 // function data(){
 //     console.log("Functions")
 // }
@@ -46,25 +49,156 @@
 
 // tassk
 
-function hello(c,d){
-    console.log(c+d)
-    console.log(c-d)
+// function hello(c,d){
+//     console.log(c+d)
+//     console.log(c-d)
+// }
+
+// hello(5,6)
+
+
+
+// let dat=function(a,b){
+//     console.log(a*b)
+//     console.log(a/b)
+// }
+// dat(2,)
+
+
+
+// function da(){
+//      console.log("sameer")
+// }
+
+// da()
+
+
+// --------------------------------------------------------
+
+
+// function with return
+
+// function add(a,b){
+//     return a+b
+// }
+// let a=add(4,6)
+// console.log(a)
+
+
+//--------------------------------------------------------
+// function add(a,b){
+//     console.log("sdas")
+//     return a+b;
+//     console.log("asbb")    //after return the line can not work
+// }
+// let a=add(4,6)
+// console.log(a)
+
+//------------------------------------------------------------
+
+// function add(){
+//     console.log("sdas")
+//     console.log("sdas")
+//     console.log("sdas")
+//     console.log("sdas")
+//     console.log("sdas")
+//     hello      //code willl get error next line does not print
+//     console.log("sdas")
+//     console.log("sdas")
+    
+// }
+
+// add()
+
+
+//----------------------------------------------------------
+
+// function add(){
+//     console.log("sdas")
+//     console.log("sdas")
+//     console.log("sdas")
+//     console.log("sdas")
+// }
+// add()
+// add()    //re duing multiple time uinsg function
+
+//==============================================================
+
+// let person={
+//     name:"abc",
+//     loc:'hyd',
+//     age:23,
+//     data:function(){
+//         console.log(`my name is ${this.name}`) 
+//     }
+// }
+
+// console.log(person)
+// console.log(person.data)
+// console.log(person.data())
+
+
+//-------------------
+
+// function add(){
+//     console.log("this is")
+// }
+
+// console.log(add())
+//---------------------------------------------------
+
+
+//arrow method
+//arrow ananymous function
+
+// let data = ()=>{
+//     console.log("this is arrow function")
+// }
+
+// data()
+
+//single paremter
+
+
+// let data = a=>{ console.log(a)}
+// data(10)
+
+// ==============================================
+
+//higer order
+// function data (hello) {
+//     console.log("hi")
+//     hello()
+//     console.log("good morining...")
+
+// }
+
+// //call back
+// function data_two(){
+//     console.log("studnets")
+
+// }
+
+// data(data_two)
+
+// recursive
+
+function fac(n){
+    if (n==0){
+
+        return 1
+    }
+    else{
+        return n*fac(n-1)
+    }
+
 }
 
-hello(5,6)
+console.log(fac(5))
+ 
 
+//iife
 
-
-let dat=function(a,b){
-    console.log(a*b)
-    console.log(a/b)
-}
-dat(2,)
-
-
-
-function da(){
-     console.log("sameer")
-}
-
-da()
+(function(){
+    console.log("this is iife")
+})()
