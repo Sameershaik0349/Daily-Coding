2daaa9ce05ae09b85a729b5bc3616c2ee3dbd5f6
Sameershaik0349 <sksameer46644@gmail.com>
2026-05-01 02:12:32 +0530

    dom

--- JS/DAY 10/app.js
+++ JS/DAY 10/app.js
@@ -27,7 +27,42 @@
 
 
 
-let p=document.getElementById('num')
-// console.log(p.parentNode )// asking who p 's parent
-// console.log(p.childNodes)
-console.log(p.nextSibling )
+// let p=document.getElementById('num')
+// // console.log(p.parentNode )// asking who p 's parent
+// // console.log(p.childNodes)
+// console.log(p.nextSibling )
+
+
+
+//maupilation=============================
+
+
+// let num =document.getElementById('num')
+// // console.log(num)
+
+// // first create a tag
+
+// let li6= document.createElement('li')
+// li6.setAttribute('id','l6')
+// li6.textContent = "six"
+// num.appendChild(li6)
+// console.log(li6)
+
+
+// let li7= document.createElement('li')
+// li7.setAttribute('id','l7')
+// li7.textContent = "seven"
+// // num.insertBefore(li7,l3)   //in specific place
+// // num.replaceChild(li7,l3)//for replce first new next old in parameter
+// num.removeChild(l3)//for remove
+// console.log(li7)
+
+// let a =document.getElementById('number')
+// console.log(a.getAttribute('class'))
+
+
+
+let hi =document.getElementById('hi')
+console.log(hi.innerText)
+console.log(hi.textContent)
+// console.log(hi.textContent())
