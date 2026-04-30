2a0c5dc2af8d7de48bf63a8a8a0d39711d31d154
Sameershaik0349 <sksameer46644@gmail.com>
2026-04-30 17:16:04 +0530

    dom

---
+++ JS/DAY 10/app.js
@@ -0,0 +1,33 @@
+// console.log(document)----url page
+// console.log(window)
+
+// document.body.style.backgroundColor ="Red"
+
+// let p =document.getElementsByTagName('ul')  //html collection
+// // console.log(p)
+// p[1].style.backgroundColor ='blue'
+
+// let p=document.getElementsByClassName("numbers1")
+// // console.log(p)
+// p[0].style.backgroundColor="yellow"
+
+
+
+// let i =document.getElementById('l1')// give direct element
+// console.log(i)
+// i.style.backgroundColor='orange'
+
+// let p= document.querySelector('ul')
+// p.style.backgroundColor='orange'
+// console.log(p)
+
+// let p=document.querySelectorAll('ul')//node list
+// // console.log(p)
+// p[0].style.backgroundColor='green'
+
+
+
+let p=document.getElementById('num')
+// console.log(p.parentNode )// asking who p 's parent
+// console.log(p.childNodes)
+console.log(p.nextSibling )
