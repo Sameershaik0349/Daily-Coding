bdd950dc2c4b61a756754138ddd1f2fe0fc542ef
Sameershaik0349 <sksameer46644@gmail.com>
2026-05-01 18:14:41 +0530

    dom

---
+++ JS/DAY 9/app.js
@@ -0,0 +1,47 @@
+const { style } = require("framer-motion/client")
+
+function add(){
+    // document.body.classList.toggle("one") --toggle
+}
+
+function trigger(){
+    console.log("product added")
+}
+function trigger(){
+    console.log("your bill is updated")
+}
+
+
+let a  =document.getElementById('lee')
+a.addEventListener('click',()=>{
+    console.log("this is on click")
+})
+
+a.addEventListener('click',()=>{
+    console.log("added")
+})
+
+
+function obj(e){
+    console.log(e)
+}
+
+
+function dbl(){
+    console.log("this is double click event")
+}
+
+// function dbl(e){
+//     console.log(e)
+// }
+
+
+function mouseover(){
+    console.log("this is mouseover")
+}
+function mouseout(){
+    console.log("this is mouseout")
+}
+function mouseup(){
+    console.log("this is mouseup")
+}
